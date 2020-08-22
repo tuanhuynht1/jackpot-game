23a9ce9fe093083e9c92afd8a0bfa1aa64dd0a12
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-08-21 20:55:19 -0400

    set up board layout

---
+++ src/app/main-page/main-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss']
+})
+export class MainPageComponent implements OnInit {
+
+  grid: Grid = new Grid();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+export class Grid {  
+
+  private grid: Circle[][];
+
+  constructor(){
+    for(let i = 0; i < 10; i++) {
+
+      this.grid.push([]);
+
+      for (let j = 0; j < 10; j++) {
+        this.grid[i].push(new Circle());
+      }
+    }
+  }
+} 
+
+
+
+export class Circle {  
+  constructor(){}
+}
