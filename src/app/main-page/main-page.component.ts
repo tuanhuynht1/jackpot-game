79cc6086ced0b25698e2d0ab918d135e7702f9c3
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-08-22 20:21:46 -0400

    begin refactoring and adding ringstate

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Direction, Color } from '../shared/enums'
 
 @Component({
   selector: 'app-main-page',
@@ -8,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class MainPageComponent implements OnInit {
 
   grid: Circle[][] = [];
-  currentIndex: Index = new Index(0,0);
-  direction: Direction = Direction.Right;
+
+  ringStates: RingState[]= [];
+
+  redIndex: Index = new Index(0,0);
+  redDirection: Direction = Direction.Right;
+
+  greenIndex: Index = new Index(1,1);
+  greenDirection: Direction = Direction.Right;
 
   constructor(){
+
+    // initialize grid
     for(let i = 0; i < 9; i++) {
 
       this.grid.push([]); 
@@ -20,60 +29,73 @@ export class MainPageComponent implements OnInit {
         this.grid[i].push(new Circle());
       }
     }
+
+    // initialize ring states
+    for(let i = 0; i < 4; i++) {
+
+      this.ringStates.push(new RingState());
+      this.ringStates[i].currentIndex = new Index(i,i);
+    }
+
+    // initialize styles
+    this.grid[0][0].style = 'red';
+    this.grid[1][1].style = 'green';
+    this.grid[2][2].style = 'blue';
+    this.grid[3][3].style = 'gold';
+
   }
 
   ngOnInit(): void {
-    setInterval(() => {
 
-      switch(this.direction){
+    // setInterval(() => {
+    //   switch(this.redDirection){
 
-        case Direction.Right:
-          this.currentIndex.j++;
-          if (this.currentIndex.j === 8) {
-            this.direction = Direction.Down;
-          }
-          break;
+    //     case Direction.Right:
+    //       this.redIndex.j++;
+    //       if (this.redIndex.j === 8) {
+    //         this.redDirection = Direction.Down;
+    //       }
+    //       break;
 
-        case Direction.Down:
-          this.currentIndex.i++;
-          if (this.currentIndex.i === 8) {
-            this.direction = Direction.Left;
-          }
-          break;
+    //     case Direction.Down:
+    //       this.redIndex.i++;
+    //       if (this.redIndex.i === 8) {
+    //         this.redDirection = Direction.Left;
+    //       }
+    //       break;
 
-        case Direction.Left:
-          this.currentIndex.j--;
-          if (this.currentIndex.j === 0) {
-            this.direction = Direction.Up;
-          }
-          break;
+    //     case Direction.Left:
+    //       this.redIndex.j--;
+    //       if (this.redIndex.j === 0) {
+    //         this.redDirection = Direction.Up;
+    //       }
+    //       break;
         
-        case Direction.Up:
-          this.currentIndex.i--;
-          if (this.currentIndex.i === 0) {
-            this.direction = Direction.Right;
-          }
-          break;
-
-      }
-
-      
-    },50);
+    //     case Direction.Up:
+    //       this.redIndex.i--;
+    //       if (this.redIndex.i === 0) {
+    //         this.redDirection = Direction.Right;
+    //       }
+    //       break;
+    //   }
+    // },50);
   }
 
 }
 
-export enum Direction {
-  Up,
-  Down,
-  Left,
-  Right
+
+
+export class RingState {
+  currentIndex: Index;
+  direction: Direction = Direction.Right;
+  intervalRate: Number = 50;
+  intervalId: Number;
 }
 
 export class Index {
   constructor(public i: number, public j: number){}
 }
 
-export class Circle {  
-  constructor(){}
+export class Circle {
+  style: string;  
 }
---
+++ src/app/shared/enums.ts
@@ -0,0 +1,13 @@
+export enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+};
+  
+export enum Color {
+    Red = 0,
+    Green = 1,
+    Blue = 2,
+    Gold = 3
+};
