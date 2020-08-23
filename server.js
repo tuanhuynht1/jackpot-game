7b6a72608cb16209242796e180b4b6294acb038d
Tuan Huynh <tuanhuynht1998@gmail.com>
2020-08-23 01:41:56 -0400

    configure for heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/budget-app'));
+app.use(express.static(__dirname + '/dist/jackpot-game'));
 
 app.get('/*', function(req,res) {
     
